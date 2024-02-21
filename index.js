bd7dd81b8421d955813e6fd0c545657e5967e569
Kotchapun Roekcharoen <kxttpw.j@gmail.com>
2024-02-21 10:45:54 +0700

    save

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const express = require('express');
     const app = express();
     var bodyParser = require('body-parser');
 
-    const base_url = "http://10.104.12.104";
+    const base_url = "http://localhost:3000";
+    //const base_url = "http://10.104.12.104";
 
     app.set("views",path.join(__dirname,"/public/views"));
     app.set('view engine','ejs');
@@ -78,8 +79,6 @@ const express = require('express');
             res.status(500).send('Error');
         }
     });
-
-
 
     app.listen(5500, () => {
         console.log('Sever started on post 5500');
